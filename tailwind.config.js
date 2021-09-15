544795cab6e2048259e519a20addc28891e55721
Jose Estrada <j.estrada.developer@gmail.com>
2021-09-15 13:58:31 -0500

    Add Hotwire Turbo Streams Turbo Frames and  Stimulus controller

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/***
+ * Excerpted from "Modern Front-End Development for Rails",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/nrclient for more book information.
+***/
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}

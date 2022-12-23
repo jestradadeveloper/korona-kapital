f7713b6c1cf264fe0988dfd657b1d22003b6bbd7
Jose Estrada <j.estrada.developer@gmail.com>
2022-12-23 17:18:05 -0600

    Add render build config

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Modern Front-End Development for Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/nrclient for more book information.
-#---
 class ConcertsController < ApplicationController
   before_action :set_concert, only: [:show, :edit, :update, :destroy]
 
@@ -61,7 +53,7 @@ class ConcertsController < ApplicationController
       if @concert.update(concert_params)
         format.html do
           render(@concert)
-          #redirect_to(@concert, notice: "Concert was successfully updated.")
+          # redirect_to(@concert, notice: "Concert was successfully updated.")
         end
 
         format.json { render(:show, status: :ok, location: @concert) }

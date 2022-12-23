f64af3a20b4460aa7e7d87c16e2a62ced06a7033
Jose Estrada <j.estrada.developer@gmail.com>
2022-12-23 17:08:29 -0600

    Update files

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,11 +1,4 @@
-#---
-# Excerpted from "Modern Front-End Development for Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/nrclient for more book information.
-#---
+
 # Define an application-wide HTTP permissions policy. For further
 # information see https://developers.google.com/web/updates/2018/06/feature-policy
 #
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Modern Front-End Development for Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/nrclient for more book information.
-#---
 GENRES = [
   "Adult Contemporary",
   "Alternative Rock",
@@ -99,7 +91,7 @@ ActiveRecord::Base.transaction do
     end
 
     concert.update(
-      genre_tags: concert.bands.flat_map(&:genres).uniq.join(","),
+      genre_tags: concert.bands.flat_map(&:genres).uniq.join(",")
     )
 
     concert.venue.rows.times do |row_number|
